0c04c6e37df2ab822585559d7f1b5cdd38fcd142
abhinand2511 <abhinand25112001@gmail.com>
2025-09-24 02:20:04 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import NotFound from "./NotFound";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
-      <div>App</div>
     </>
   );
 };
